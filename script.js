3dc7dbd5a9ea16cd189691c2137f35bb7fef1fbc
Matthew <matthewchaplin60@gmail.com>
2021-08-14 20:50:38 +0900

    Added Button functionality

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 let compChoice = "Nothing";
 let playerChoice = "Paper";
 
+let score = 0;
+let plays = 0;
+let loss = 0;
+
+let scoreText = document.getElementById("score");
+let winLossText = document.getElementById("WinLoss");
 
 function computerPlay () {
     const throwNumber = Math.floor(Math.random() * 3) +1;
@@ -27,33 +33,39 @@ function computerPlay () {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return 0;
+        
     } else {
+        
         if (playerSelection == "rock" && computerSelection == "paper") {
             return 1;
         }
         if (playerSelection == "paper" && computerSelection == "rock") {
             return 1;
         }
-        if (playerSelection == "sciccors" && computerSelection == "paper") {
+        if (playerSelection == "scissors" && computerSelection == "paper") {
             return 1;   
         }
     }
-
     return 0;
 }
 
-
-function game() {
-    let score = 0;
-    for (let i = 0; i < 5; i++) {
-        compChoice = computerPlay();
-        score += playRound(playerChoice.toLowerCase(), compChoice);
+function playGame(playerSelection) {
+    let result = playRound(playerSelection, computerPlay());
+    console.log(result);
+    plays ++;
+    if (result == 1) {
+        console.log("You win!");
+        score ++;
+        scoreText.innerText = `Current Score: ${score}`;
+        winLossText.innerText = `Win/Lose Ratio: ${score} / ${plays}`;
     }
-
-    console.log(`Your total score is: ${score}/5`);
-    alert(`Your total score is: ${score}/5`)
+    else{
+        console.log("You Lose!");
+        winLossText.innerText = `Win/Lose Ratio: ${score} / ${plays}`;
+    }
 }
 
-playerChoice = prompt("Please choose Rock/Paper/Sciccors");
-game();
+document.getElementById("rockBtn").addEventListener("click", function(){playGame("rock");});
+document.getElementById("paperBtn").addEventListener("click", function(){playGame("paper");});
+document.getElementById("scissorBtn").addEventListener("click", function(){playGame("scissors");});
 
